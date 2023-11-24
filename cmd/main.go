43a928d95ca80c699186c7423c0a6a971cddb904
Rafael Fernando Souza Ribeiro <rafaelribeirosouza86@gmail.com>
2023-11-24 12:30:16 -0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,21 +5,17 @@ import (
 )
 
 func main() {
-	arr := []string{"1.1", "1.2"}
 	//maps := map[interface{}]interface{}{"1": 1, 1.5: 1.6, "4": 3}
-	// count := GetTamanho[map[int]string, int, string](map[int]string{1: "1", 2: "2"})
-	// println(count)
-	//var k interface{}
-	//k = 1
-	//result := Result{Value: "valor", Type: "TIPO", TypeFull: "TIPOFULL"}
-	sucess, err := array.ArrayHasValue("1.1", arr)
+	count := array.GetTamanho[map[int]string, int, string](map[int]string{1: "1", 2: "2"})
+	println(count)
+
+	arr := []string{"1.1", "1.2"}
+	sucess, err := array.ArrayHasValue("1.3", arr)
 
 	if err != nil {
 		panic(err)
-	} 
-	
-	println(sucess)
+	}
 
-	// result := GetValueType(map[string]int{"rafa": 1, "bbb": 2})
-	// fmt.Printf("Type: %s,Type Inteiro: %s, Value: %v\n", result.Type, result.TypeFull, result.Value)
+	println(sucess)
+	
 }
