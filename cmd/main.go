53473f445b9224a3db6a5038712573e147d7cf79
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2023-12-08 01:34:11 -0300

    Fixing duplication of return results

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,7 @@ func main() {
 	//maps := map[interface{}]interface{}{"1": 1, 1.5: 1.6, "4": 3}
 	// count := array.GetTamanho[map[int]string, int, string](map[int]string{1: "1", 2: "2"})
 	// println(count)
+	//
 
 	// arr := []string{"1.1", "1.2"}
 	// sucess, err := array.ArrayHasValue("1.2", arr)
